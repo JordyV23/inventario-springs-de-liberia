810cc69f1b006adb106a028607695825fd10ae25
JordyV23 <jordyvargas0123@gmail.com>
2024-06-24 09:53:54 -0600

    Inicializacion del Frontend
     - Se crea el proyecto del frontend
    - Se instalan dependencias necesarias
    - Se eliminan archivos innecesarios

---
+++ frontend/src/InventarioApp.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Button } from "flowbite-react";
+
+function InventarioApp() {
+  return (
+    <>
+      <Button>Hola Mundo</Button>
+    </>
+  );
+}
+
+export default InventarioApp;
